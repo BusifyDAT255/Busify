3a49b8d6b896b54df9c05deec255575ca495aa35
David Genelöv <david.genelov@gmail.com>
2016-05-05 00:08:54 +0200

    Add javadoc to MainActivity.java

--- app/src/main/java/com/example/eliasvensson/busify/DateDialog.java
+++ app/src/main/java/com/example/eliasvensson/busify/DateDialog.java
@@ -1,11 +1,12 @@
-/*
- * Made 2015-05-04 by Elias Svensson and David Genelov
+/**
+ * @author Elias Svensson and David Genelov
+ * @version 1.0, 2015-05-04
  * This class was made with the aid of tutorials available at
  * http://developer.android.com/guide/topics/ui/controls/pickers.html#DatePicker
  *
  * DialogFragments are typically used to display a dialog window floating on top of its activity
  * window. DateDialog extends DialogFragment, and the dialog window in this particular case
-  * appears as a "DatePicker", which lets the user pick a date from a calendar.
+ * appears as a "DatePicker", which lets the user pick a date from a calendar.
  *
  * Furthermore, when a date is selected by a user, the method "OnDateSet" defined in the interface
  * OnDateSetListener is called. This method saves the specified date as a String, and sets it as
@@ -26,7 +27,7 @@ import java.util.Calendar;
 
 public class DateDialog extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
-    /** Variable that stores which EditText-object that the date will be written to  */
+    // Variable that stores which EditText-object that the date will be written to
     private EditText textToChange;
 
     /** Constructor that assigns which EditText-object the date will be written to
@@ -40,14 +41,13 @@ public class DateDialog extends DialogFragment implements DatePickerDialog.OnDat
      * @return A new instance of DatePickerDialog with todays date as default
      * @param savedInstanceState The current state of the application
      *
-     * */
+     */
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
-        
+
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
@@ -56,8 +56,7 @@ public class DateDialog extends DialogFragment implements DatePickerDialog.OnDat
      * @param year The selected year
      * @param month The selected month
      * @param day The selected day
-     *
-     * */
+     */
     public void onDateSet(DatePicker view, int year, int month, int day) {
         //show to the selected date in the text box
         String date=day+"-"+(month+1)+"-"+year;
--- app/src/main/java/com/example/eliasvensson/busify/MainActivity.java
+++ app/src/main/java/com/example/eliasvensson/busify/MainActivity.java
@@ -1,3 +1,17 @@
+/**
+ * @author Elias Svensson and David Genelov
+ * @version 1.0, 2015-05-04
+ *
+ * Manages the interaction with, and function of, the main view of the app.
+ * The main screen consists of a "Welcome" label, a "hint-label" to guide the user in
+ * how to use the app, two different textfields (start date and end date), one button for each
+ * textfield to set the date, and one button to send a .csv file
+ *
+ * The user simply chooses a start and an end date by clicking the buttons, and then clicks the
+ * "send .csv"-button.
+ *
+ * Full functionality is not yet implemented.
+ */
 
 package com.example.eliasvensson.busify;
 
@@ -9,39 +23,50 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    /** Initiates all the functionality in the app when it is started
+     * @param savedInstanceState The current state of the app
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Sets the view to be displayed upon the start of the app
         setContentView(R.layout.activity_main);
 
-        Button fromDateButton = (Button) findViewById(R.id.from_date_button);
-        Button toDateButton = (Button) findViewById(R.id.to_date_button);
+        // Initiates the buttons for setting start and end date
+        Button fromDateButton = (Button) findViewById(R.id.start_date_button);
+        Button toDateButton = (Button) findViewById(R.id.end_date_button);
 
+        // Initiates a View.OnClickListener to listen for clicks on the buttons
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
 
-            /**   */
+            /** Lets the user specify a date, and updates the text of a view to match that date.
+             * @param v The view which will get its text updated
+             *
+             */
             public void onClick(View v) {
-                if (v == findViewById(R.id.from_date_button))
+                if (v == findViewById(R.id.start_date_button))
                     setDateToView(R.id.txt_from_date);
 
-                if (v == findViewById(R.id.to_date_button))
+                else if (v == findViewById(R.id.end_date_button))
                     setDateToView(R.id.txt_to_date);
             }
 
             /**
-             *This method creates an instance of the class DateDialog,
-             *
+             *This method creates an instance of the class DateDialog, which does the
              * @param  viewId  the ID of the view which the method will write the returned date to.
              *
              */
             private void setDateToView(int viewId){
+                //Initiates a DateDialog object for user interaction when choosing the date
                 DateDialog dialog = new DateDialog(findViewById(viewId));
                 FragmentTransaction ft = getFragmentManager().beginTransaction();
                 dialog.show(ft, "DatePicker");
             }
         };
 
+        //Assigns the pre-defined listener to listen to the two buttons
         fromDateButton.setOnClickListener(listener);
         toDateButton.setOnClickListener(listener);
     }
