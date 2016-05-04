d482fba0f03a8f71e9f227e837b05d8becdc1fd1
David Genelöv <david.genelov@gmail.com>
2016-05-04 11:29:03 +0200

    First working datepicker for ToDate

--- app/src/main/java/com/example/eliasvensson/busify/MainActivity.java
+++ app/src/main/java/com/example/eliasvensson/busify/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.eliasvensson.busify;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onStart() {
+        super.onStart();
+
+        EditText txtDate = (EditText) findViewById(R.id.txt_to_date);
+        txtDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View view, boolean hasfocus) {
+                if (hasfocus) {
+                    DateDialog dialog = new DateDialog(view);
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    dialog.show(ft, "DatePicker");
+
+                }
+            }
+
+        });
+    }
+
 }
