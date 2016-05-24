f96d496ecf9aaf2c3eb2e76b6b690d90e5b26ed2
Jonathan Fager <faaager@gmail.com>
2016-05-24 13:22:54 +0200

    Finally the bloody links work as they are supposed to. Done with async call to Firebase API

--- app/src/main/java/com/example/eliasvensson/busify/MainActivity.java
+++ app/src/main/java/com/example/eliasvensson/busify/MainActivity.java
@@ -19,13 +19,17 @@ package com.example.eliasvensson.busify;
 
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -34,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button sendButton;
     Button dateButton;
+    String attachmentLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,9 +59,10 @@ public class MainActivity extends AppCompatActivity {
                     setDateToView(R.id.txt_date);
                 else if (v == findViewById(R.id.send_button)){
                     //Returns a link to the file corresponding to the chosen date
-                    String attachmentLink = niceLink(((EditText)findViewById(R.id.txt_date)).getText().toString());
-                    //Opens Androids default mail-app with a link to the above file attached.
-                    sendEmail(attachmentLink);
+                    String callDate = ((EditText)findViewById(R.id.txt_date)).getText().toString();
+                    Log.d("Datum", callDate);
+                    getUrlAsync(callDate);
+
                 }
             }
         };
@@ -66,16 +72,24 @@ public class MainActivity extends AppCompatActivity {
         sendButton.setOnClickListener(listener);
     }
 
+    private void setDownloadLink(Uri link){
+
+        attachmentLink = link.toString();
+    }
+    private String getDownloadLink(){
+
+        return attachmentLink;
+    }
     /**
      * Opens Androids default mail-application with a link to a file attached.
-     * @param  attachmentLink The link to the file
+     * The link to the file
      *
      */
-    private void sendEmail(String attachmentLink){
+    private void sendEmail(){
         Intent i = new Intent(Intent.ACTION_SEND);
         i.setType("message/rfc822");
         i.putExtra(Intent.EXTRA_SUBJECT, "Your ElectriCity report");
-        i.putExtra(Intent.EXTRA_TEXT   , attachmentLink);
+        i.putExtra(Intent.EXTRA_TEXT   , getDownloadLink());
         try {
             startActivity(Intent.createChooser(i, "Send mail..."));
         } catch (android.content.ActivityNotFoundException ex) {
@@ -97,51 +111,22 @@ public class MainActivity extends AppCompatActivity {
         dialog.show(ft, "DatePicker");
     }
 
-    /**
-     * Returns a link to a specific .csv-file, corresponding to a date input
-     * @param date The date to find a file for
-     * @return A String with the link corresponding to the date for the chosen file
-     */
-    /**
-    private String chooseURL (String date) {
-        String link;
-        switch(date){
-            case "19-5-2016":
-                link = "https://firebasestorage.googleapis.com/v0/b/dat255-busify.appspot.com/o/2016-05-19.csv?alt=media&token=20520547-18b7-458d-9019-e1dc3cdd83cd";
-                break;
-            case "20-5-2016":
-                link = "https://firebasestorage.googleapis.com/v0/b/dat255-busify.appspot.com/o/2016-05-20.csv?alt=media&token=ed2ee38e-c97a-4d81-ae5f-9824f842cfed";
-                break;
-            case "21-5-2016":
-                link = "https://firebasestorage.googleapis.com/v0/b/dat255-busify.appspot.com/o/2016-05-21.csv?alt=media&token=4b247cec-23ea-4522-a99a-e573d550230f";
-                break;
-            case "22-5-2016":
-                link = "https://firebasestorage.googleapis.com/v0/b/dat255-busify.appspot.com/o/2016-05-22.csv?alt=media&token=f05266c0-3281-4112-8a4e-1fbf96bd6929";
-                break;
-            case "23-5-2016":
-                link = "https://firebasestorage.googleapis.com/v0/b/dat255-busify.appspot.com/o/2016-05-23.csv?alt=media&token=222481d8-3d30-4069-b40d-4f631855a437";
-                break;
-            case "24-5-2016":
-                link = "https://firebasestorage.googleapis.com/v0/b/dat255-busify.appspot.com/o/2016-05-24.csv?alt=media&token=6daa827c-8bbe-4189-999a-e89f21b3f483";
-                break;
-            case "25-5-2016":
-                link ="https://firebasestorage.googleapis.com/v0/b/dat255-busify.appspot.com/o/2016-05-25.csv?alt=media&token=e4ea105a-40c8-439d-9006-56514a22dfcf";
-                break;
-            default:
-                link ="Report missing for chosen date";
-                break;
-        }
-        return link;
-    }
-**/
-    private String niceLink (String date){
-        String link;
-        // Points to the root reference
-        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
-        StorageReference dateRef = storageRef.child("/" + date + ".csv");
-        link = dateRef.getDownloadUrl().toString();
-        return link;
-    }
+   // Calls the server to securely obtain an unguessable download Url
+   private void getUrlAsync (String date){
+       Task<Uri> link;
+       // Points to the root reference
+       StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+       StorageReference dateRef = storageRef.child("/" + date + ".csv");
+       link = dateRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>()
+       {
+           @Override
+           public void onSuccess(Uri downloadUrl)
+           {
+               setDownloadLink(downloadUrl);
+               sendEmail();
+           }
+       });
+   }
 
 
 }
