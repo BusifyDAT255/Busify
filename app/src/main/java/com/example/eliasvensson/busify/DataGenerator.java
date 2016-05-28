828a742e797a9ff2c487ba66397377f5271de733
anniesoderstrom <sannie@student.chalmers.se>
2016-05-28 13:24:34 +0200

    add inherent messages

--- app/src/main/java/com/example/eliasvensson/busify/DataGenerator.java
+++ app/src/main/java/com/example/eliasvensson/busify/DataGenerator.java
@@ -9,12 +9,14 @@
 package com.example.eliasvensson.busify;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
@@ -60,29 +62,28 @@ public class DataGenerator {
 
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-
+                Log.d("Child added: ", dataSnapshot.getKey().toString());
             }
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                Log.e("The string is: ", s);
-                Log.e("Changed bus type to: ", dataSnapshot.getValue().toString());
+                Log.d("Child changed: ", dataSnapshot.getKey().toString());
 
             }
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
-
+                Log.d("Child removed: ", dataSnapshot.getKey().toString());
             }
 
             @Override
             public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
+                Log.d("Child moved: ", dataSnapshot.getKey().toString());
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Log.w("Cancelled: ", databaseError.getMessage(), databaseError.toException());
             }
         });
 
