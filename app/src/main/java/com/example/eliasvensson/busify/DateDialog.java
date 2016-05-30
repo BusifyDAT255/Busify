38573c011d26db8c1b6099b58a5e8d515a52bb89
David Genelöv <david.genelov@gmail.com>
2016-05-30 13:36:28 +0200

    Update javadoc to new version

--- app/src/main/java/com/example/eliasvensson/busify/DateDialog.java
+++ app/src/main/java/com/example/eliasvensson/busify/DateDialog.java
@@ -1,6 +1,7 @@
 /**
- * @author Elias Svensson and David Genelov
- * @version 1.0, 2016-05-04
+ * @author Elias Svensson
+ * @author David Genelöv
+ * @version 7.0, 2016-05-30
  * @since 1.0
  * This class was made with the aid of tutorials available at
  * http://developer.android.com/guide/topics/ui/controls/pickers.html#DatePicker
