12075e88c158f6f9ebab9282c5a71914de68acbe
RegularSvensson <elias.svensson.1992@gmail.com>
2016-05-31 10:14:05 +0200

    Update comments

--- app/src/main/java/com/example/eliasvensson/busify/CsvHandler.java
+++ app/src/main/java/com/example/eliasvensson/busify/CsvHandler.java
@@ -34,8 +34,8 @@ public class CsvHandler {
     }
 
     /**
-     * Method that writes a two-dimensional array with strings, to a .csv-file with a specified
-     * date as the filename.
+     * Method that writes a two-dimensional array with string fields,
+     * to a string on .csv-format with a specified date as the filename.
      *
      * @param dataArray array to write to a .csv
      * @param callDate specified date that gets passed to the filename
@@ -44,8 +44,7 @@ public class CsvHandler {
         String filename = callDate + ".csv";
         //Creates the String which will make up the text for the .csv
         String csvText = "";
-        //Adds all elements in Array to the string
-        //TODO: Make sure this parses the text correctly to .csv-file format (dependent on Sara & Annies method)
+        // parse 2D array to string in .csv format
         for (int i = 0; i < dataArray.length; i++) {
             for (int j = 0; j < dataArray[0].length; j++) {
                 csvText += dataArray[i][j] + ", ";
