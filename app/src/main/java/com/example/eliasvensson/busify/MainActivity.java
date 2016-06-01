f59d67799a94a124497abd289ab5e9bc4a490f03
JonathanFager <faaager@gmail.com>
2016-06-01 20:41:36 +0200

    removed unused imprts

--- app/src/main/java/com/example/eliasvensson/busify/MainActivity.java
+++ app/src/main/java/com/example/eliasvensson/busify/MainActivity.java
@@ -35,7 +35,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
