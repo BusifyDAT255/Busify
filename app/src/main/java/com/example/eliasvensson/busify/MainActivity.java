9904c37e860a3425123d396c70fe3d6161b1bc7a
David Genelöv <david.genelov@gmail.com>
2016-06-01 18:04:48 +0200

    Change date to reportDate in MainActivity

--- app/src/main/java/com/example/eliasvensson/busify/MainActivity.java
+++ app/src/main/java/com/example/eliasvensson/busify/MainActivity.java
@@ -8,9 +8,11 @@
  * @version 10.0, 2016-06-01
  * @since 1.0
  * <p/>
- * Manages the interaction with, and function of, the main view of the app.
+ * This class works as the engine of the app. It manages the interaction with all other classes
+ * associated, and changes the appearance for the main view of the app
+ * <p/>
  * The main screen consists of a "Welcome" label, a "hint-label" to guide the user in
- * how to use the app, a date button to set the date and one button to send a .csv file
+ * how to use the app, a date button to set the date and a button to share a .csv file
  * <p/>
  * The user simply chooses a date by clicking the date-button.
  * When pressing the send button, the default android mail-application starts with a
@@ -18,7 +20,11 @@
  * The default email contains a link to a .csv file which can then be accessed by the recipient
  * of the email.
  * <p/>
- * Note: The class works as intended, but is in need of some serious refactoring.
+ * This app uses Firebase as a database and storage for .csv-files.
+ * <p/>
+ * NOTE: Data is only available for the 18th to the 24th of May 2016.
+ * NOTE 2: If there is no existing file for the chosen date, a file will be created and uploaded to
+ *         the Firebase storage. Otherwise, it will return the link to the already existing file.
  */
 
 package com.example.eliasvensson.busify;
@@ -42,9 +48,7 @@ import com.google.firebase.storage.StorageReference;
 public class MainActivity extends AppCompatActivity {
 
     /**
-     * Defines variables for the DatePicker button, the button used to share
-     * the link and the link attached in the email to be sent.
-     * A storage reference and DatabaseHandler is defined.
+     * Defines all the variables the app will need for execution.
      */
     private Button shareButton;
     private Button dateButton;
@@ -60,32 +64,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Initializes a DatabaseHandler
-        databaseHandler = new DatabaseHandler(11, 5);
-
         // Sets the view to be displayed upon the start of the app
         setContentView(R.layout.activity_main);
 
-        // Creates a CsvHandler object for handling everything concerning .csv-files
+        // Initializes a DatabaseHandler
+        databaseHandler = new DatabaseHandler(11, 5);
+
+        // Initializes a CsvHandler object for handling everything concerning .csv-files
         csvHandler = new CsvHandler(MainActivity.this);
 
-        // Creates an EmailHandler for handling everything related to email sending
+        // Initializes an EmailHandler for handling everything related to email sending
         emailHandler = new EmailHandler(MainActivity.this);
 
-        //Creates a new StorageHandler for handling everything with Firebase storage
+        // Initializes a new StorageHandler for handling all calls to the Firebase storage
         storageHandler = new StorageHandler();
 
-        // Initiates a ProgressDialog, for use when the app is fetching data from database
+        // Initializes a ProgressDialog, for use when the app is fetching data from database
         progressDialog = new ProgressDialog(this);
 
-        // Initiates the buttons for setting date and sharing the link
+        // Initializes the buttons for setting date and sharing the link
         dateButton = (Button) findViewById(R.id.date_button);
         shareButton = (Button) findViewById(R.id.share_button);
 
-        //Initiate text view
+        // Initializes the text view, where the date will appear
         reportDateText = (EditText) findViewById(R.id.txt_date);
 
-        // Initiates a View.OnClickListener to listen for clicks on the dateButton and shareButton
+        // Initializes a View.OnClickListener to listen for clicks on the dateButton and shareButton
         View.OnClickListener listener = clickHandler();
 
         // Assigns the pre-defined listener to listen to the buttons
@@ -102,21 +106,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (v == dateButton)
+                    // Opens a date dialog to let the user pick a date
                     openDateDialog();
-                else if (v == shareButton) {
 
+                else if (v == shareButton) {
                     // Starts the database connection
                     databaseHandler.initiateDatabase(reportDate);
 
-                    // Starts the progressDialog bar
-                    progressDialog.setMessage("Generating report");
+                    // Starts the progressDialog
+                    progressDialog.setMessage("Generating report, please wait.");
                     progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
                     progressDialog.setIndeterminate(true);
                     progressDialog.show();
 
                     // Finds or creates the URL for the specified date, and opens a mail-application
                     // with the link attached
-                    shareReport(reportDate);
+                    shareReport();
                 }
             }
         };
@@ -127,19 +132,18 @@ public class MainActivity extends AppCompatActivity {
      * using an async call.
      * onSuccess sets the the downloadLink by call to setDownloadLink
      * and initiates the email by call to sendEmail
-     * onFailure opens a dialog telling the user that no report is available for this date.*
-     * @param date should be in the format of "YYYY-MM-DD"
+     * onFailure opens a dialog telling the user that no report is available for this date.
      */
-    private void shareReport(final String date) {
+    private void shareReport() {
 
         // Make a reference to the date-specific file on storage
-        storageHandler.setDateStorageReference("/reports/" + date + ".csv");
+        storageHandler.setDateStorageReference("/reports/" + reportDate + ".csv");
 
         // Get the reference to the date-specific file on storage
-        StorageReference dateRef = storageHandler.getDateStorageReference();
+        StorageReference dateReference = storageHandler.getDateStorageReference();
 
         // Try to get the file from Firebase storage, based on the reference defined above
-        dateRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+        dateReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
 
             /**
              * If the specified file exists on storage, find its URL and attach it to an email
@@ -148,9 +152,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(Uri downloadUrl) {
                 Log.e("LOG", "file already existed.");
+
+                // If an URI was found, convert it to a string
                 String downloadLink = downloadUrl.toString();
 
-                // Stops the progressDialog bar from running
+                // Stops the progressDialog from running
                 progressDialog.cancel();
 
                 // Sends an email with the URL attached
@@ -166,13 +172,13 @@ public class MainActivity extends AppCompatActivity {
             public void onFailure(@NonNull Exception e) {
 
                 //Create a .csv-file and upload to Firebase storage
-                buildCsv(date);
+                buildCsv(reportDate);
 
                 // Upload the file to Firebase storage
                 storageHandler.uploadFile(csvHandler.getFilePath(reportDate));
 
                 // Recursive call to share the report
-                shareReport(reportDate);
+                shareReport();
             }
         });
     }
